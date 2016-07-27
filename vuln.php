18739b3206a04ef86e58df43769802edf7aee794
gxtaillon <gx@taillon.co>
2016-07-27 18:04:12 -0400

    Staging modifications

--- vuln.php
+++ vuln.php
@@ -25,7 +25,14 @@ function vuln_activation() {
 register_deactivation_hook(__FILE__, 'vuln_deactivation');
 function vuln_deactivation() {
   wp_clear_scheduled_hook('vuln_cron_file_log_event');
-} 
+}
+
+add_action('template_redirect', 'vuln_wp_error_detection');
+function vuln_wp_error_detection() {
+  if (is_404()) {
+    
+  }
+}
 
 function vuln_pdo_print_file_log($pdo) {
   $sql = '
@@ -166,6 +173,70 @@ function vuln_pdo_create_tables($pdo) {
         on delete cascade
         on update cascade
     );
+    CREATE TABLE `vuln_probing_event` (
+      `id` int unsigned not null auto_increment,
+      `when` timestamp not null,
+      `who` varchar(255) not null,
+      primary key (`id`),
+    );
+    // http://www.artfulsoftware.com/infotree/qrytip.php?id=552
+    DELIMITER GO
+    CREATE FUNCTION levenshtein( s1 VARCHAR(255), s2 VARCHAR(255) )
+      RETURNS INT
+      DETERMINISTIC
+      BEGIN
+        DECLARE s1_len, s2_len, i, j, c, c_temp, cost INT;
+        DECLARE s1_char CHAR;
+        -- max strlen=255
+        DECLARE cv0, cv1 VARBINARY(256);
+        SET s1_len = CHAR_LENGTH(s1), s2_len = CHAR_LENGTH(s2), cv1 = 0x00, j = 1, i = 1, c = 0;
+        IF s1 = s2 THEN
+          RETURN 0;
+        ELSEIF s1_len = 0 THEN
+          RETURN s2_len;
+        ELSEIF s2_len = 0 THEN
+          RETURN s1_len;
+        ELSE
+          WHILE j <= s2_len DO
+            SET cv1 = CONCAT(cv1, UNHEX(HEX(j))), j = j + 1;
+          END WHILE;
+          WHILE i <= s1_len DO
+            SET s1_char = SUBSTRING(s1, i, 1), c = i, cv0 = UNHEX(HEX(i)), j = 1;
+            WHILE j <= s2_len DO
+              SET c = c + 1;
+              IF s1_char = SUBSTRING(s2, j, 1) THEN 
+                SET cost = 0; ELSE SET cost = 1;
+              END IF;
+              SET c_temp = CONV(HEX(SUBSTRING(cv1, j, 1)), 16, 10) + cost;
+              IF c > c_temp THEN SET c = c_temp; END IF;
+                SET c_temp = CONV(HEX(SUBSTRING(cv1, j+1, 1)), 16, 10) + 1;
+                IF c > c_temp THEN 
+                  SET c = c_temp; 
+                END IF;
+                SET cv0 = CONCAT(cv0, UNHEX(HEX(c))), j = j + 1;
+            END WHILE;
+            SET cv1 = cv0, i = i + 1;
+          END WHILE;
+        END IF;
+        RETURN c;
+      END
+    GO
+    CREATE FUNCTION levenshtein_ratio( s1 VARCHAR(255), s2 VARCHAR(255) )
+      RETURNS INT
+      DETERMINISTIC
+      BEGIN
+        DECLARE s1_len, s2_len, max_len INT;
+        SET s1_len = LENGTH(s1), s2_len = LENGTH(s2);
+        IF s1_len > s2_len THEN 
+          SET max_len = s1_len; 
+        ELSE 
+          SET max_len = s2_len; 
+        END IF;
+        RETURN ROUND((1 - LEVENSHTEIN(s1, s2) / max_len) * 100);
+      END
+    GO
+    DELIMITER ;
+    ;
   " ;
   try {
     $pdo->exec($sql);
